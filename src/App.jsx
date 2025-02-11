606987c399ff24b456ba90a866d4fb6c124c3352
Dasonchris <dasonchris47@gmail.com>
2025-02-11 10:55:19 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Todo from "./Todo";
+import Form from "./Form";
+import FilterButton from "./FilterButton";
+import PropTypes from "prop-types";
+import { nanoid } from "nanoid";
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: `todo-0${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const taskList = tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+
+  const headingText = `${tasks.length} remaining`;
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>Todo Mate</h1>
+      <Form addTask={addTask} />
+
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+App.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default App;
